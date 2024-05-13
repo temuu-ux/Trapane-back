75fddd70a9152f3f9de3e77639428da2af54fd85
temuu-ux <temuulenb80@gmail.com>
2024-05-13 14:44:31 +0800

    wow

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,27 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Accept",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
